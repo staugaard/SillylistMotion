99113a8404a4e248f004ca9d4d83260c6cb666b8
Mick Staugaard <mick@staugaard.com>
2012-05-10 00:30:29 -0700

    just some comments

--- app/list_controller.rb
+++ app/list_controller.rb
@@ -1,14 +1,17 @@
 class ListController < Kondi::TableViewController
+  # required
   def size
     TodoItem.count
   end
 
+  # required method for constructing the cells in the table
   def cell_for(index_path)
     ListCell.create(self).tap do |cell|
       cell.todo_item = TodoItem.all[index_path.row]
     end
   end
 
+  # optional method called when a cell is selected
   def selected(index_path)
     cell_at(index_path).toggle
   end
